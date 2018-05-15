f6177454a30b6fda7180fed44191bdf7fbc474a7
Ryan Padilha <ryan.padilha@gmail.com>
2018-05-15 17:49:19 -0300

    scripts

--- bin/start.sh
+++ bin/start.sh
@@ -16,5 +16,7 @@ source venv-sandbox/bin/activate
 PID=/var/company/run/gunicorn-flask-origins.pid
 if [ -f $PID ]; then rm $PID; fi
 
+mkdir -p /var/company/logs/flask-origins
+
 to_console "start python application"
 exec gunicorn -w 3 --bind=0.0.0.0:9000 --user=ubuntu --log-level=debug --pid=$PID --log-file=/var/company/logs/flask-origins/gunicorn.log 2>>/var/company/logs/flask-origins/gunicorn.log wsgi:application &
